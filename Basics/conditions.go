24076a4cb65a82268a0d0a166098ba54391022dd
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-01-23 16:37:16 +0500

    Conditions
    Worked with conditions, changed readline and learned how to use one variable in the second if it has another type by converting. For example if x has int8 type and value = 2. You want to use it in the y variable which has int64 type. You can assign x to y in case if you will convert initial x type int8 to int64 by var y int64 = int64(x)

---
+++ Basics/conditions.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := -3
+	if num > 0 {
+		fmt.Println("Number is greater than 0")
+	} else if num < 0 {
+		fmt.Println("Number is less than 0")
+	} else if num == 3 {
+		fmt.Println("Number equals 3 !!! ")
+	}
+}
--- Basics/readline.go
+++ Basics/readline.go
@@ -11,6 +11,11 @@ func main() {
 	var name string
 	var age uint8
 
+	//Change variables type in another var
+	var x int8 = 2
+	var y int64 = int64(x)
+	fmt.Println(y)
+
 	//Name
 	fmt.Println("What is your name?")
 	fmt.Scan(&name)
@@ -19,5 +24,5 @@ func main() {
 	//Age
 	fmt.Println("So, how old are you?")
 	fmt.Scan(&age)
-	fmt.Println("Wow, you are " + fmt.Sprint(age) + " years!")
+	fmt.Println("Wow, you are " + fmt.Sprint(age) + " years old!")
 }
