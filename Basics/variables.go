bf7ea4cb363f7c2fe98db19eb19b88af18c1b33c
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-01-23 14:54:22 +0500

    Variables added

---
+++ Basics/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Variables")
+
+	//Vars
+	//var "var_name" type
+
+	//Int8 with age
+	var age int8
+
+	//Float32
+	var floatvar float32 = 12
+
+	//String
+	var name string = "Alex"
+}
