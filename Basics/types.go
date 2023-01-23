5cc6d9f99797576b8f7f7c42e4307ecdc524f945
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-01-23 16:42:06 +0500

    Removed bloat, created types.go to summary all types in the Golang

---
+++ Basics/types.go
@@ -0,0 +1,28 @@
+/* MAIN TYPES
+
+
+
+string
+
+bool
+
+int -
+	int int8 int16 int32 int64
+
+	uint uint8 uint16 uint32 uint64 uinitptr
+
+	byte - alias for uint8
+
+	rune - alias for int32
+
+	float32 float64
+
+	complex64 complex128
+
+*/
+
+package main
+
+func main() {
+
+}
