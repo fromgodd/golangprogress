88e353c6eb237539e327faa5a97393b67d515ae3
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-01-23 15:35:47 +0500

    changed readline, learned Sprint command, readline by scan

--- Basics/readline.go
+++ Basics/readline.go
@@ -1,10 +1,23 @@
+//Read line by scan
+//Sprint command to convert int to string
+
 package main
 
 import "fmt"
 
 func main() {
+
+	//Variables
 	var name string
+	var age uint8
+
+	//Name
 	fmt.Println("What is your name?")
 	fmt.Scan(&name)
 	fmt.Println("Hi, " + name + "!")
+
+	//Age
+	fmt.Println("So, how old are you?")
+	fmt.Scan(&age)
+	fmt.Println("Wow, you are " + fmt.Sprint(age) + " years!")
 }
