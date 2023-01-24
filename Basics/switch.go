6abd9ad4f328c2f9defcaddc32d11ea99dbfd4ed
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-01-24 18:29:41 +0500

    Learned new switch cases! TODO LEARN FALLTHROUGH

--- Basics/switch.go
+++ Basics/switch.go
@@ -28,4 +28,23 @@ func main() {
 		fmt.Println("Name is Jordan")
 	}
 
+	/*
+		If we want check if number is lower or higher than X in switch case, then we shouldn't write <var> after switch word. Directly open case and condition.
+
+		switch {
+			number > 10 {
+				...
+			}
+		}
+
+	*/
+	//TODO: CHECK FALLTHROUGH
+	number := 2
+	switch {
+	case number > 5:
+		fmt.Println("Number is higher than 5")
+		fallthrough
+	case number < 5:
+		fmt.Println("Number is lower than 5")
+	}
 }
