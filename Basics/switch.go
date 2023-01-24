bf14e4dc1fb55ed593cfe8c81d8f645d83469c74
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-01-24 17:29:04 +0500

    Switch updates

--- Basics/switch.go
+++ Basics/switch.go
@@ -2,15 +2,30 @@
 
 package main
 
+import "fmt"
+
 func main() {
 	name := "John"
 	// if name == "Kate" {
 	// 	fmt.Println("it is not john!")
 	// }
 
-	//switch <object>
+	/*
+		switch <var> {
+			case "something":
+				condition
+
+			default: // like in C++
+		}
+	*/
 
 	switch name {
-
+	case "Jordan":
+		fmt.Println("Name is JORDAN")
+	case "Kate":
+		fmt.Println("Name is KATE")
+	default:
+		fmt.Println("Name is Jordan")
 	}
+
 }
