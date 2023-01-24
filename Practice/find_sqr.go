33d8fff5f24871a73396e2ddcbbdbe0cd8167e69
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-01-24 14:26:50 +0500

    Rewritten Find Square Equation

--- Practice/find_sqr.go
+++ Practice/find_sqr.go
@@ -6,38 +6,36 @@ import (
 )
 
 func main() {
+	var a, b, c float64
+	fmt.Println("Find roots of square equation")
 
-	//ax^2 + bx + c = 0
-
-	var a, b, c, x1, x2 float64
-
-	fmt.Println("Find square of equation")
-	fmt.Println("Input A ")
+	fmt.Println("Enter A: ")
 	fmt.Scan(&a)
-	fmt.Println("Input B ")
+
+	fmt.Println("Enter B: ")
 	fmt.Scan(&b)
-	fmt.Println("Input C")
+
+	fmt.Println("Enter C: ")
 	fmt.Scan(&c)
 
 	D := (b * b) - 4*(a*c)
+	fmt.Println("Discriminant is " + fmt.Sprint(D))
 
 	if D > 0 {
-		//var x1 float64
-		//var x2 float64
+		var x1 float64
+		var x2 float64
+		fmt.Println("Equation has 2 roots")
 		x1 = (-b + math.Sqrt(D)) / (2 * a)
 		x2 = (-b - math.Sqrt(D)) / (2 * a)
-		// fmt.Println("The equation has two roots.\nX1 -> " + fmt.Sprint(x1) + "\nX2 -> " + fmt.Sprint(x2))
-		fmt.Println("X1: " + fmt.Sprint(x1) + "\nX2: " + fmt.Sprint(x2) + "\nDiscriminant = " + fmt.Sprint(D))
+		fmt.Println("X1 = " + fmt.Sprint(x1))
+		fmt.Println("X2 = " + fmt.Sprint(x2))
 	} else if D == 0 {
-		//var x1 float64
-		//var x2 flaot64
 		var x float64
 		x = (-b) / (2 * a)
-		fmt.Println("Equation has one root")
-		fmt.Println("X: " + fmt.Sprint(x))
-	} else if D < 0 {
-		fmt.Println("Equation has no roots! D<0")
-		fmt.Println("Discriminant is " + fmt.Sprint(D))
-	}
+		fmt.Println("Equation has single root")
+		fmt.Println("X = " + fmt.Sprint(x))
 
+	} else if D < 0 {
+		fmt.Println("Equation has no solution!")
+	}
 }
