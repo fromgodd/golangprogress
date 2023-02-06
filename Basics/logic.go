91ed3f55bd5afdddbb2afaf1187c649e98cdbe24
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-02-06 17:34:53 +0500

    worked with math library

--- Basics/logic.go
+++ Basics/logic.go
@@ -9,14 +9,16 @@ func main() {
 	/*
 		AND - &&
 		OR - ||
-		NOR -
+		NOR - !=
 
 	*/
 	if a > 1 || b > 11 {
 		fmt.Println("True")
 	}
-	if a < 3 && b < 24 {
+	if a < 10 && b < 24 {
 		println("Second")
 	}
-
+	if a != 5 {
+		println("hello, world")
+	}
 }
---
+++ Basics/math.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+basic math operations
++ - * /
+
+to divide evenly use static variable type (float64)
+
+
+% - remainder from division
+
+*/
+
+func main() {
+	var a float64 = 144
+	result := math.Sqrt(a)
+
+	fmt.Println(result)
+
+}
