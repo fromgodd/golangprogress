a577acb54c38ef47dec581b42e8c1835516baf9a
Said Archer <97128346+saidofficial@users.noreply.github.com>
2023-02-06 18:51:13 +0500

    todo fix

--- Basics/math.go
+++ Basics/math.go
@@ -14,12 +14,22 @@ to divide evenly use static variable type (float64)
 
 % - remainder from division
 
+
+math library
+
+Ceil - round to highest (everything) 25.2 -> 26 25.0005 -> 26, 25.9999 -> 26
+
+Floor
+
+
 */
 
 func main() {
-	var a float64 = 144
-	result := math.Sqrt(a)
-
+	var a float64 = 25.2135
+	result := math.Ceil(a)
 	fmt.Println(result)
+	var b float64 = 25.322115
+	result2 := math.Floor(b)
+	fmt.Println(result2)
 
 }
